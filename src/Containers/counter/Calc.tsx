a35e795b0cacb1ec6ce6de2efc597d18c747aefd
Kwon Valerii <wellko24@gmail.com>
2022-12-15 18:28:56 +0600

    added checks for NaN undefined and other errors

--- src/Containers/counter/Calc.tsx
+++ src/Containers/counter/Calc.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../app/store";
-import {ClearDisplay, DeleteLastChar, DisplayChange, Result} from "./counterSlice";
+import {ClearDisplay, DeleteLastChar, DisplayChange, Result, SymbolChange} from "./counterSlice";
 import './Calc.css';
 
 const Calc = () => {
     const dispatch = useDispatch();
     const counterValue = useSelector((state: RootState) => state.counter.value);
+    const buttonDisable = useSelector((state: RootState) => state.counter.disable)
     return (
         <div className='calc'>
             <div className='display'>{(counterValue !== '')? counterValue: '0'}</div>
@@ -17,15 +18,15 @@ const Calc = () => {
             <button className='calcBtn' onClick={() => dispatch(DisplayChange('4'))}>4</button>
             <button className='calcBtn' onClick={() => dispatch(DisplayChange('5'))}>5</button>
             <button className='calcBtn' onClick={() => dispatch(DisplayChange('6'))}>6</button>
-            <button className='calcBtn' onClick={() => dispatch(DisplayChange('-'))}>-</button>
+            <button className='calcBtn' disabled={buttonDisable} onClick={() => dispatch(SymbolChange('-'))}>-</button>
             <button className='calcBtn' onClick={() => dispatch(DisplayChange('7'))}>7</button>
             <button className='calcBtn' onClick={() => dispatch(DisplayChange('8'))}>8</button>
             <button className='calcBtn' onClick={() => dispatch(DisplayChange('9'))}>9</button>
-            <button className='calcBtn' onClick={() => dispatch(DisplayChange('+'))}>+</button>
+            <button className='calcBtn' disabled={buttonDisable} onClick={() => dispatch(SymbolChange('+'))}>+</button>
             <button className='calcBtn' onClick={() => dispatch(DisplayChange('0'))}>0</button>
-            <button className='calcBtn' onClick={() => dispatch(DisplayChange('/'))}>/</button>
-            <button className='calcBtn' onClick={() => dispatch(DisplayChange('*'))}>*</button>
-                 <button className='calcBtn' onClick={() => dispatch(Result())}>=</button>
+            <button className='calcBtn' disabled={buttonDisable} onClick={() => dispatch(SymbolChange('/'))}>/</button>
+            <button className='calcBtn' disabled={buttonDisable} onClick={() => dispatch(SymbolChange('*'))}>*</button>
+                 <button className='calcBtn' disabled={buttonDisable} onClick={() => dispatch(Result())}>=</button>
             <button className='calcBtn' onClick={() => dispatch(ClearDisplay())}>C</button>
         </div>
     );
