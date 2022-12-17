a3a954f8bac6497d89db6790e669a3932699e375
Kwon Valerii <wellko24@gmail.com>
2022-12-17 11:56:18 +0600

    changed checks

--- src/Containers/counter/Calc.tsx
+++ src/Containers/counter/Calc.tsx
@@ -5,31 +5,31 @@ import {ClearDisplay, DeleteLastChar, DisplayChange, Result, SymbolChange} from 
 import './Calc.css';
 
 const Calc = () => {
-    const dispatch = useDispatch();
-    const counterValue = useSelector((state: RootState) => state.counter.value);
-    const buttonDisable = useSelector((state: RootState) => state.counter.disable)
-    return (
-        <div className='calc'>
-            <div className='display'>{(counterValue !== '')? counterValue: '0'}</div>
-            <button className='calcBtn' onClick={() => dispatch(DisplayChange('1'))}>1</button>
-            <button className='calcBtn' onClick={() => dispatch(DisplayChange('2'))}>2</button>
-            <button className='calcBtn' onClick={() => dispatch(DisplayChange('3'))}>3</button>
-            <button className='calcBtn' onClick={() => dispatch(DeleteLastChar())}> DEL</button>
-            <button className='calcBtn' onClick={() => dispatch(DisplayChange('4'))}>4</button>
-            <button className='calcBtn' onClick={() => dispatch(DisplayChange('5'))}>5</button>
-            <button className='calcBtn' onClick={() => dispatch(DisplayChange('6'))}>6</button>
-            <button className='calcBtn' disabled={buttonDisable} onClick={() => dispatch(SymbolChange('-'))}>-</button>
-            <button className='calcBtn' onClick={() => dispatch(DisplayChange('7'))}>7</button>
-            <button className='calcBtn' onClick={() => dispatch(DisplayChange('8'))}>8</button>
-            <button className='calcBtn' onClick={() => dispatch(DisplayChange('9'))}>9</button>
-            <button className='calcBtn' disabled={buttonDisable} onClick={() => dispatch(SymbolChange('+'))}>+</button>
-            <button className='calcBtn' onClick={() => dispatch(DisplayChange('0'))}>0</button>
-            <button className='calcBtn' disabled={buttonDisable} onClick={() => dispatch(SymbolChange('/'))}>/</button>
-            <button className='calcBtn' disabled={buttonDisable} onClick={() => dispatch(SymbolChange('*'))}>*</button>
-                 <button className='calcBtn' disabled={buttonDisable} onClick={() => dispatch(Result())}>=</button>
-            <button className='calcBtn' onClick={() => dispatch(ClearDisplay())}>C</button>
-        </div>
-    );
+	const dispatch = useDispatch();
+	const counterValue = useSelector((state: RootState) => state.counter.value);
+	const buttonDisable = useSelector((state: RootState) => state.counter.disable)
+	return (
+		<div className='calc'>
+			<div className='display'>{(counterValue !== '') ? counterValue : '0'}</div>
+			<button className='calcBtn' onClick={() => dispatch(DisplayChange('1'))}>1</button>
+			<button className='calcBtn' onClick={() => dispatch(DisplayChange('2'))}>2</button>
+			<button className='calcBtn' onClick={() => dispatch(DisplayChange('3'))}>3</button>
+			<button className='calcBtn' onClick={() => dispatch(DeleteLastChar())}> DEL</button>
+			<button className='calcBtn' onClick={() => dispatch(DisplayChange('4'))}>4</button>
+			<button className='calcBtn' onClick={() => dispatch(DisplayChange('5'))}>5</button>
+			<button className='calcBtn' onClick={() => dispatch(DisplayChange('6'))}>6</button>
+			<button className='calcBtn' disabled={buttonDisable} onClick={() => dispatch(SymbolChange('-'))}>-</button>
+			<button className='calcBtn' onClick={() => dispatch(DisplayChange('7'))}>7</button>
+			<button className='calcBtn' onClick={() => dispatch(DisplayChange('8'))}>8</button>
+			<button className='calcBtn' onClick={() => dispatch(DisplayChange('9'))}>9</button>
+			<button className='calcBtn' disabled={buttonDisable} onClick={() => dispatch(SymbolChange('+'))}>+</button>
+			<button className='calcBtn' onClick={() => dispatch(DisplayChange('0'))}>0</button>
+			<button className='calcBtn' disabled={buttonDisable} onClick={() => dispatch(SymbolChange('/'))}>/</button>
+			<button className='calcBtn' disabled={buttonDisable} onClick={() => dispatch(SymbolChange('*'))}>*</button>
+			<button className='calcBtn' disabled={buttonDisable} onClick={() => dispatch(Result())}>=</button>
+			<button className='calcBtn' onClick={() => dispatch(ClearDisplay())}>C</button>
+		</div>
+	);
 };
 
 export default Calc;
--- src/Containers/counter/counterSlice.tsx
+++ src/Containers/counter/counterSlice.tsx
@@ -1,65 +1,65 @@
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 interface CalcDisplayState {
-    value: string;
-    disable: boolean;
+	value: string;
+	disable: boolean;
 }
 
 const initialState: CalcDisplayState = {
-    value: '',
-    disable: true,
+	value: '',
+	disable: true,
 }
 
 export const CalcSlice = createSlice({
-    name: 'counter',
-    initialState,
-    reducers: {
-        DisplayChange: (state, action: PayloadAction<string>) => {
-            if (action.payload !== '0') {
-                state.value = state.value + action.payload;
-                state.disable = false;
-            } else {
-                const checkerDev = state.value.split('/');
-                const checkerSumm = state.value.split('+');
-                const checkerMinus = state.value.split('-');
-                const checkerMult = state.value.split('*')
-                if (checkerDev[checkerDev.length - 1] !== '' &&
-                    checkerSumm[checkerSumm.length - 1] !== '' &&
-                    checkerMinus[checkerMinus.length - 1] !== '' &&
-                    checkerMult[checkerMult.length - 1] !== '') {
-                    state.value = state.value + action.payload;
-                    state.disable = false;
-                }
-            }
-        },
+	name: 'counter',
+	initialState,
+	reducers: {
+		DisplayChange: (state, action: PayloadAction<string>) => {
+			if (action.payload !== '0') {
+				state.value = state.value + action.payload;
+				state.disable = false;
+			} else {
+				const checkerDev = state.value.split('/');
+				const checkerSumm = state.value.split('+');
+				const checkerMinus = state.value.split('-');
+				const checkerMult = state.value.split('*')
+				if (checkerDev[checkerDev.length - 1] !== '' &&
+					checkerSumm[checkerSumm.length - 1] !== '' &&
+					checkerMinus[checkerMinus.length - 1] !== '' &&
+					checkerMult[checkerMult.length - 1] !== '') {
+					state.value = state.value + action.payload;
+					state.disable = false;
+				}
+			}
+		},
 
-        SymbolChange: (state, action: PayloadAction<string>) => {
-            state.value = state.value + action.payload;
-            state.disable = true;
-        },
+		SymbolChange: (state, action: PayloadAction<string>) => {
+			state.value = state.value + action.payload;
+			state.disable = true;
+		},
 
-        Result: (state) => {
-            state.value = eval(state.value).toString();
-            console.log(parseInt(state.value))
-            if (/[a-zA-Z]/.test(state.value)) {
-                state.value = '';
-                state.disable = true;
-            }
-        },
+		Result: (state) => {
+			state.value = eval(state.value).toString();
+			console.log(parseInt(state.value))
+			if (/[a-zA-Z]/.test(state.value)) {
+				state.value = '';
+				state.disable = true;
+			}
+		},
 
-        ClearDisplay: (state) => {
-            state.value = '';
-            state.disable = true;
-        },
+		ClearDisplay: (state) => {
+			state.value = '';
+			state.disable = true;
+		},
 
-        DeleteLastChar: (state) => {
-            state.value = state.value.slice(0, -1);
-            if (state.value.length > 1){
-                const lastChar = state.value[(state.value.length - 1)]
-                state.disable = lastChar === '/' || lastChar === '*' || lastChar === '+' || lastChar === '-';
-            }
-        }
-    },
+		DeleteLastChar: (state) => {
+			state.value = state.value.slice(0, -1);
+			if (state.value.length >= 0) {
+				const lastChar = state.value[(state.value.length - 1)]
+				state.disable = !(/[0-9]/.test(lastChar));
+			}
+		}
+	},
 })
 
 export const counterReducer = CalcSlice.reducer;
